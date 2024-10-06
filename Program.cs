d2c966040ff3e327081047744c4bfacce6f2226d
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2024-10-06 23:17:44 +0500

    Implemented expandProductionRules()
    expandProductionRules() divides all production rules to <lhs>, list<rhs>
    and stores them in a dictionary. All the first and follow set
    calculation will be done on that list.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,40 @@
 {
     public class Program
     {
+        
+        private static Dictionary<String, List<String>> productionRules = new Dictionary<String, List<String>>();
+        private static String[]? rulesList;
+
+        public static void expandProductionRules()
+        {
+            foreach (String prodRule in rulesList)
+            {
+                String[] rule = prodRule.Split("->", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                System.Diagnostics.Debug.Assert(rule.Length == 2, "rule length must be 2!");
+                List<String> rhs = rule[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+                if (!productionRules.ContainsKey(rule[0]))
+                {
+                    productionRules.Add(rule[0], rhs);
+                } else 
+                {
+                    foreach (String rightSideRule in rhs)
+                    {
+                        if (productionRules[rule[0]].Contains(rightSideRule))
+                        {
+                            continue;
+                        }
+                        productionRules[rule[0]].Add(rightSideRule);
+                    }
+                }
+            }
+        }
+
         public static void Main(String[] args) 
         {
-            String[] rulesList = File.ReadAllLines("grammar.txt");
+            rulesList = File.ReadAllLines("grammar.txt");
+            expandProductionRules();
+    
+
         }
     }
 }
