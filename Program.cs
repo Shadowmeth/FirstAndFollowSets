9e1a9f53076b010bc14e80e4d27437fcb24ff648
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2024-10-06 23:42:55 +0500

    Modified expandProductionRules() function
    Old logic was not able to differentiate between different right hand
    side production rules. New method requires more string processing when
    calculating first and follow sets but at least we have the right
    representation now.

--- Program.cs
+++ Program.cs
@@ -2,40 +2,46 @@
 {
     public class Program
     {
-        
-        private static Dictionary<String, List<String>> productionRules = new Dictionary<String, List<String>>();
+        private static Dictionary<String, List<String>> productionRules =
+            new Dictionary<String, List<String>>();
         private static String[]? rulesList;
 
         public static void expandProductionRules()
         {
             foreach (String prodRule in rulesList)
             {
-                String[] rule = prodRule.Split("->", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                String[] rule = prodRule.Split(
+                    "->",
+                    StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+                );
                 System.Diagnostics.Debug.Assert(rule.Length == 2, "rule length must be 2!");
-                List<String> rhs = rule[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+                String rhs = rule[1].Trim();
                 if (!productionRules.ContainsKey(rule[0]))
                 {
-                    productionRules.Add(rule[0], rhs);
-                } else 
+                    List<String> temp = new List<String>();
+                    temp.Add(rhs);
+                    productionRules.Add(rule[0], temp);
+                }
+                else
                 {
-                    foreach (String rightSideRule in rhs)
-                    {
-                        if (productionRules[rule[0]].Contains(rightSideRule))
-                        {
-                            continue;
-                        }
-                        productionRules[rule[0]].Add(rightSideRule);
-                    }
+                    productionRules[rule[0]].Add(rhs);
                 }
             }
         }
 
-        public static void Main(String[] args) 
+        public static void Main(String[] args)
         {
             rulesList = File.ReadAllLines("grammar.txt");
             expandProductionRules();
-    
 
+            foreach (KeyValuePair<String, List<String>> kvp in productionRules)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (String prodRule in kvp.Value)
+                {
+                    Console.WriteLine("\t" + prodRule);
+                }
+            }
         }
     }
 }
